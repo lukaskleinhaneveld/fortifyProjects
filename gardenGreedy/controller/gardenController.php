a162bd1dffbb481ead3dbaa1ae330083ca999a81
Lukas Klein Haneveld <sakulkh@gmail.com>
2017-04-07 14:36:50 +0200

    Eerste push: Login pagina gemaakt en gestyled; PHP nog niets aan gedaan. Ook register en wachtwoord vergeten pagina zijn aangemaakt maar nog niet gestyled of iets.

---
+++ gardenGreedy/controller/gardenController.php
@@ -0,0 +1,25 @@
+<?php
+
+function index(){
+    
+    
+	render("garden/index");	
+}
+
+function login(){
+    
+    
+    render("garden/login");
+}
+
+function register(){
+    
+    
+    render("garden/register");
+}
+
+function forgotten(){
+    
+    
+    render("garden/forgotten");
+}
